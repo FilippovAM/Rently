7fc9a3675bbaedb1525dfa40054176e38709c333
FilippovAM <cyberross@rambler.ru>
2016-01-25 00:17:50 +0800

    add new files

--- js/rently.js
+++ js/rently.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
+
+    swiper();
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        grabCursor: true,
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    })
+    function swiper() {
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            grabCursor: true,
+            pagination: '.swiper-pagination',
+            paginationClickable: true
+        });
+    }
 });
+
+function formCard() {
+    $('.form-card').card({
+        container: '.card-wrapper',
+        formSelectors: {
+            numberInput: 'input[name="number"]',
+            nameInput: 'input[name="full-name"]',
+            expiryInput: 'input[name="expiry"]',
+            cvcInput: 'input[name="cvc"]'
+        },
+        placeholders: {
+            number: '**** **** **** ****',
+            name: 'Arya Stark',
+            expiry: '**/****',
+            cvc: '***'
+        }
+    });
+}
+
+formCard();
+
