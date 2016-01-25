3d3e04f66e4e47c6644ad74afb65ecfb07dd09d3
FilippovAM <cyberross@rambler.ru>
2016-01-26 01:10:52 +0800

    add, rubber

--- js/rently.js
+++ js/rently.js
@@ -1,36 +1,71 @@
 $(document).ready(function () {
 
     swiper();
-    //initialize swiper when document ready
+    modalShow();
+    emailResult();
+
     function swiper() {
-        var mySwiper = new Swiper ('.swiper-container', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: true,
-            grabCursor: true,
-            pagination: '.swiper-pagination',
-            paginationClickable: true
+        $('.swiper-container').is(function () {
+            var mySwiper = new Swiper(this, {
+                // Optional parameters
+                direction: 'horizontal',
+                loop: true,
+                grabCursor: true,
+                pagination: '.swiper-pagination',
+                paginationClickable: true
+            });
         });
+    }
+
+    $('.registration-button').on('click', function () {
+        modalShow();
+    });
+
+    function modalShow() {
+        var a = location.href;
+        var number = $('.form-card #number').attr('name');
+        var name = $('.form-card #full-name').attr('name');
+        var expiry = $('.form-card #expiry').attr('name');
+        var cvc = $('.form-card #cvc').attr('name');
+
+        if (a.indexOf(number) != -1 && a.indexOf(name) != -1 && a.indexOf(expiry) != -1 && a.indexOf(cvc) != -1) {
+            if (a.indexOf(number + '=&') == -1 && a.indexOf(name + '=&') == -1 && a.indexOf(expiry + '=&') == -1 && a.indexOf(cvc + '=' + ' ') == -1) {
+                $('#modal').modal('show');
+            }
+        }
+
     }
 });
 
+formCard();
+
 function formCard() {
-    $('.form-card').card({
-        container: '.card-wrapper',
-        formSelectors: {
-            numberInput: 'input[name="number"]',
-            nameInput: 'input[name="full-name"]',
-            expiryInput: 'input[name="expiry"]',
-            cvcInput: 'input[name="cvc"]'
-        },
-        placeholders: {
-            number: '**** **** **** ****',
-            name: 'Arya Stark',
-            expiry: '**/****',
-            cvc: '***'
-        }
+    $('.form-card').is(function () {
+        $(this).card({
+            container: '.card-wrapper',
+            formSelectors: {
+                numberInput: 'input[name="number"]',
+                nameInput: 'input[name="full-name"]',
+                expiryInput: 'input[name="expiry"]',
+                cvcInput: 'input[name="cvc"]'
+            },
+            placeholders: {
+                number: '**** **** **** ****',
+                name: 'Arya Stark',
+                expiry: '**/****',
+                cvc: '***'
+            }
+        });
+    });
+
+}
+
+function emailResult() {
+    $('#email-result').is( function(){
+        $(this).val($('#agent-email').html());
     });
 }
 
-formCard();
+
+
 
